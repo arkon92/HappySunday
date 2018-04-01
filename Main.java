805c80d84c719d92c7028bfa5968f0e2d48468d2
arkon92 <root@DESKTOP-1LOKU88.localdomain>
2018-04-01 19:39:13 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+private class Main implements Comparator<Point>
+    {
+        public int compare(Point p1, Point p2) {
+            double slope1 = slopeTo(p1);
+            double slope2 = slopeTo(p2);
+
+            if (slope1 < slope2) return -1;
+            if (slope2 < slope1) return 1;
+            return 0;
+        }
