8a1920595db1e9044d4364364b66813fe81e02c4
Anya75 <toto@exemple.com>
2018-04-01 19:49:14 +0200

    my first commit

--- Main.java
+++ Main.java
@@ -3,8 +3,11 @@ private class Main implements Comparator<Point>
         public int compare(Point p1, Point p2) {
             double slope1 = slopeTo(p1);
             double slope2 = slopeTo(p2);
+			
+			int slope3 = 9;
 
             if (slope1 < slope2) return -1;
             if (slope2 < slope1) return 1;
             return 0;
         }
+	}
