874b906f9922f57a187870ebd4b7ed1edce327d5
Your Name <you@example.com>
2018-12-26 19:13:40 +0100

    Solve conflict

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ private class Main implements Comparator<Point>
         public int compare(Point p1, Point p2) {
             double slope1 = slopeTo(p1);
             double slope2 = slopeTo(p2);
-		// Add comment hello
+
+		// Add comment ti create conflict
+
             if (slope1 < slope2) return -1;
             if (slope2 < slope1) return 1;
             return 0;
