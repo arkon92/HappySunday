851b4953cf9b21342055ee38a0b7de7e645374a3
lucedebole <luce_debole@yahoo.com>
2018-12-26 19:08:01 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ private class Main implements Comparator<Point>
         public int compare(Point p1, Point p2) {
             double slope1 = slopeTo(p1);
             double slope2 = slopeTo(p2);
-
+// add something
             if (slope1 < slope2) return -1;
             if (slope2 < slope1) return 1;
             return 0;
